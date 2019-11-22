b9797c90662b21fdfd174bdd23fb24c1cf64905e
CAMills-git <55893919+CAMills-git@users.noreply.github.com>
2019-11-22 15:00:10 -0500

    Update mitocarta.R

--- data-raw/mitocarta.R
+++ data-raw/mitocarta.R
@@ -10,6 +10,11 @@ mitocarta <- read_tsv(url, col_names = TRUE) %>%
   rename(MCARTA2_score = MCARTA2.0_score) %>%
   clean_names()
 
+mitocarta$hg19_chromosome<- str_replace(as.matrix(mitocarta$hg19_chromosome), "chr", "")
+
+chromosome_levels <- c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "X", "Y", "M")
+mitocarta$hg19_chromosome<- fct_relevel(mitocarta$hg19_chromosome, chromosome_levels)
+
 mitocarta$MCARTA2_LIST <- if_else(mitocarta$MCARTA2_LIST == 1, TRUE, FALSE)
 
 #run to generate csv; commented out due to size
